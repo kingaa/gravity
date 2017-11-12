809e155e6aff7b0ef82025bac0fc71e6f4496048
Aaron A. King <kingaa@umich.edu>
2017-11-11 20:56:18 -0500

    reorganize

--- tsir.R
+++ tsir.R
@@ -19,65 +19,42 @@ library(doMC)
 registerDoMC()
 
 ## ----clean-data,cache=FALSE----------------------------------------------
-bake(file="ew_measles_data.rds",{
-  library(aakmisc)
-  
-  startTunnel()
-  
-  getQuery("select town,year,births,pop from demog where year>=1944 order by town,year") -> demog
+library(aakmisc)
 
-  getQuery("select town,date,cases from measles where year>=1944 order by town,date") %>%
-    mutate(year=as.integer(format(date+3,"%Y"))) %>%
-    ddply(~town+year,mutate,week=seq_along(year),biweek=(week+1)%/%2) %>%
-    subset(week<=52,select=-c(date,week)) %>%
-    acast(town~year~biweek,value.var="cases",fun.aggregate=sum) %>%
-    melt(varnames=c("town","year","biweek"),value.name="cases") %>%
-    mutate(town=as.character(town)) %>%
-    arrange(town,year,biweek) %>%
-    join(demog,by=c("town","year")) %>%
-    mutate(births=births/26) -> measles
-  
-  stopTunnel()
-  measles
-}) -> dat
+startTunnel()
 
-## ----distances,cache=FALSE-----------------------------------------------
-bake(file="distances.rds",{
-  library(aakmisc)
-  
-  startTunnel()
-  getQuery("select * from coords") %>% arrange(town) -> coords
-  stopTunnel()
-  rownames(coords) <- coords$town
-  
-  library(geosphere)
-  distm(coords %>% subset(select=c(long,lat)), 
-        coords %>% subset(select=c(long,lat))) %>%
-    matrix(nrow=nrow(coords),ncol=nrow(coords),
-           dimnames=list(coords$town,coords$town))
-}) -> distances
+getQuery("select town,year,births,pop from demog where year>=1944 order by town,year") -> demog
+
+getQuery("select town,date,cases from measles where year>=1944 order by town,date") %>%
+  mutate(year=as.integer(format(date+3,"%Y"))) %>%
+  ddply(~town+year,mutate,week=seq_along(year),biweek=(week+1)%/%2) %>%
+  subset(week<=52,select=-c(date,week)) %>%
+  acast(town~year~biweek,value.var="cases",fun.aggregate=sum) %>%
+  melt(varnames=c("town","year","biweek"),value.name="cases") %>%
+  mutate(town=as.character(town)) %>%
+  arrange(town,year,biweek) %>%
+  join(demog,by=c("town","year")) %>%
+  mutate(births=births/26) -> dat
+
+stopTunnel()
 
 ## ----under-reporting-----------------------------------------------------
-estUR <- function (dat, df = 2.5) {
-  cumbirths <- cumsum(dat$births)
-  cumcases <- cumsum(dat$cases)
-  fit <- smooth.spline(cumbirths,cumcases,df=df)
-  mutate(dat,
+foreach (d=dlply(dat,~town),.combine=rbind,.inorder=FALSE) %dopar% {
+  cumbirths <- cumsum(d$births)
+  cumcases <- cumsum(d$cases)
+  fit <- smooth.spline(cumbirths,cumcases,df=2.5)
+  mutate(d,
          ur=predict(fit,x=cumbirths,deriv=1)$y,
          I=cases/ur)
-}
-
-dat %>% ddply(~town,estUR,.parallel=TRUE) -> dat
+} -> dat
 
 ## ----susceptible-reconstruction------------------------------------------
-suscRecon <- function (dat, df = 2.5) {
-  cumbirths <- cumsum(dat$births)
-  cuminc <- cumsum(dat$I)
-  fit <- smooth.spline(cumbirths,cuminc,df=df)
-  mutate(dat,z=-residuals(fit))
-}
-
-dat %>% ddply(~town,suscRecon,.parallel=TRUE) -> dat
+foreach (d=dlply(dat,~town),.combine=rbind,.inorder=FALSE) %dopar% {
+  cumbirths <- cumsum(d$births)
+  cuminc <- cumsum(d$I)
+  fit <- smooth.spline(cumbirths,cuminc,df=2.5)
+  mutate(d,z=-residuals(fit))
+} -> dat
 
 ## ----make-data-matrix----------------------------------------------------
 dat %>%
@@ -98,48 +75,49 @@ dat %>%
 print(length(excludes))
 
 ## ----sigma-profile,cache=FALSE-------------------------------------------
-stew("sigma-profile.rda",{
-    
-    dat %>% subset(!(town%in%excludes)) -> dat1
-    
-    sigma1dev <- function (dat, sigma) {
-        slag <- with(dat,sigma*Ps+zlag)
-        fit <- glm(log(I)~-1+seas+log(Ilag)+I(1/Ilag)+offset(log(slag)),
-                   data=dat,subset=Ilag>0&I>0)
-        fit$deviance
-    }
-    
-    foreach (sigma=seq(0.03,0.25,length=100),
-             .combine=rbind,.inorder=FALSE) %dopar%
-        {
-            dat1 %>% 
-                daply(~town,sigma1dev,sigma=sigma,.parallel=TRUE) %>% 
-                sum() -> dev
-            data.frame(sigma=sigma,dev=dev)
-        } -> sigmaProf
-    
-    fit <- optim(par=0.037,lower=0.03,upper=0.10,
-                 method="Brent",hessian=TRUE,
-                 fn=function (sigma) {
-                     dat1 %>% 
-                         daply(~town,sigma1dev,sigma=sigma,.parallel=TRUE) %>% 
-                         sum()
-                 })
-    fit$par -> sigmaBar
+dat %>% subset(!(town%in%excludes)) -> dat1
 
-    rm(dat1)
+sigma1dev <- function (dat, sigma) {
+  slag <- with(dat,sigma*Ps+zlag)
+  fit <- glm(log(I)~-1+seas+log(Ilag)+I(1/Ilag)+offset(log(slag)),
+             data=dat,subset=Ilag>0&I>0)
+  fit$deviance
+}
 
-    dat %>% mutate(Slag=sigmaBar*Ps+zlag) -> dat
-})
+foreach (sigma=seq(0.03,0.25,length=100),
+         .combine=rbind,.inorder=FALSE) %dopar%
+         {
+           dat1 %>%
+             daply(~town,sigma1dev,sigma=sigma,.parallel=TRUE) %>%
+             sum() -> dev
+           data.frame(sigma=sigma,dev=dev)
+         } -> sigmaProf
+
+sigmaProf %>% saveRDS("sigma-profile.rds")
+
+fit <- optim(par=0.037,lower=0.03,upper=0.10,
+             method="Brent",hessian=TRUE,
+             fn=function (sigma) {
+               dat1 %>%
+                 daply(~town,sigma1dev,sigma=sigma,.parallel=TRUE) %>%
+                 sum()
+             })
+fit$par -> sigmaBar
+
+rm(dat1)
+
+sigmaBar %>% saveRDS("sigma-bar.rds")
+
+dat %>% mutate(Slag=sigmaBar*Ps+zlag) -> dat
 
 ## ----fit-tsir,cache=FALSE------------------------------------------------
 bake(file="tsir-fits.rds",{
   coefnames <- c(sprintf("seas%d",1:26),"log(Ilag)","I(1/Ilag)")
   newcoefnames <- c(sprintf("log.beta%02d",1:26),"alpha","m.alpha")
-  
+
   tsirfit <- function (dat) {
     glm(log(I)~-1+seas+log(Ilag)+I(1/Ilag)+offset(log(Slag)),
-        data=dat,subset=Ilag>0&I>0) %>% summary() %>% 
+        data=dat,subset=Ilag>0&I>0) %>% summary() %>%
       extract2("coefficients") -> fit
     fit[,"Estimate"] %>% extract(coefnames) %>% as.list() %>% as.data.frame() %>%
       set_names(newcoefnames) -> coefs
@@ -148,31 +126,47 @@ bake(file="tsir-fits.rds",{
     cbind(coefs,se,sigma=sigmaBar,
           town=unique(dat$town),Ps=unique(dat$Ps))
   }
-  
-  dat %>% ddply(~town,tsirfit,.parallel=TRUE) %>% 
-    melt(id=c("town","Ps")) %>% 
+
+  dat %>% ddply(~town,tsirfit,.parallel=TRUE) %>%
+    melt(id=c("town","Ps")) %>%
     mutate(se=ifelse(grepl("\\.se$",variable),"se","est"),
            variable=sub("\\.se$","",variable)) %>%
     dcast(town+Ps+variable~se) -> tsirs
-  
+
   dat %>% ddply(~town,summarize,Ps=unique(Ps)) -> tsircoef
-  
-  tsirs %>% 
+
+  tsirs %>%
     na.omit() %>%
     ddply(~variable, function (d) {
-      fit <- lm(est~log(Ps),data=d,weights=1/se^2) 
+      fit <- lm(est~log(Ps),data=d,weights=1/se^2)
       data.frame(town=tsircoef$town,value=predict(fit,newdata=tsircoef))
-    },.parallel=TRUE) %>% 
-    dcast(town~variable) %>% 
+    },.parallel=TRUE) %>%
+    dcast(town~variable) %>%
     melt(id=c("town","alpha","m.alpha"),value.name="log.beta") %>%
     mutate(biweek=as.integer(sub("log.beta","",as.character(variable)))) %>%
     arrange(town,biweek) %>%
     subset(select=-variable) -> tsircoef
-  
+
   dat %>%
     join(tsircoef,by=c("town","biweek")) %>%
     mutate(ylag=Ilag/Ps) -> dat
 }) -> dat
+
+## ----distances,cache=FALSE-----------------------------------------------
+bake(file="distances.rds",{
+  library(aakmisc)
+
+  startTunnel()
+  getQuery("select * from coords") %>% arrange(town) -> coords
+  stopTunnel()
+  rownames(coords) <- coords$town
+
+  library(geosphere)
+  distm(coords %>% subset(select=c(long,lat)),
+        coords %>% subset(select=c(long,lat))) %>%
+    matrix(nrow=nrow(coords),ncol=nrow(coords),
+           dimnames=list(coords$town,coords$town))
+}) -> distances
 
 ## ----session-info-------------------------------------------------------
 sessionInfo()
